978ea58882d64790278b0d06da11edb63d578719
ImEden6 <mervyn.h.mh@gmail.com>
2026-03-11 18:01:03 +0800

    better nullsafety errors, better performance over iteratorion allocation, change regen redirect to modifyarg

--- src/main/java/com/mervyn/effectsrebalancer/mixin/RegenerationMixin.java
+++ src/main/java/com/mervyn/effectsrebalancer/mixin/RegenerationMixin.java
@@ -5,17 +5,16 @@ import net.minecraft.entity.LivingEntity;
 import net.minecraft.entity.effect.StatusEffect;
 import org.spongepowered.asm.mixin.Mixin;
 import org.spongepowered.asm.mixin.injection.At;
-import org.spongepowered.asm.mixin.injection.Redirect;
+import org.spongepowered.asm.mixin.injection.ModifyArg;
 
 @Mixin(StatusEffect.class)
 public class RegenerationMixin {
 
-    @Redirect(method = "applyUpdateEffect(Lnet/minecraft/entity/LivingEntity;I)V", at = @At(value = "INVOKE", target = "Lnet/minecraft/entity/LivingEntity;heal(F)V"))
-    public void modifyRegenAmount(LivingEntity instance, float amount) {
-        if (((StatusEffect) (Object) this).getTranslationKey().equals("effect.minecraft.regeneration")) {
-            instance.heal(SyncedConfig.regenerationAmount);
-        } else {
-            instance.heal(amount);
+    @ModifyArg(method = "applyUpdateEffect(Lnet/minecraft/entity/LivingEntity;I)V", at = @At(value = "INVOKE", target = "Lnet/minecraft/entity/LivingEntity;heal(F)V"), index = 0)
+    public float modifyRegenAmount(float amount) {
+        if ("effect.minecraft.regeneration".equals(((StatusEffect) (Object) this).getTranslationKey())) {
+            return SyncedConfig.regenerationAmount;
         }
+        return amount;
     }
 }
