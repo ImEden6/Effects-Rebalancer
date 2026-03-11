6415336769d5b04c5965e77b19b5d7f3e5ab2a30
Stalemated <52725146+Stalemated@users.noreply.github.com>
2026-03-11 13:23:31 -0600

    Added support for custom resistance formula and fixed some resistance logic

--- src/main/java/com/mervyn/effectsrebalancer/config/SyncedConfig.java
+++ src/main/java/com/mervyn/effectsrebalancer/config/SyncedConfig.java
@@ -3,6 +3,7 @@ package com.mervyn.effectsrebalancer.config;
 public class SyncedConfig {
     // Volatile to ensure thread safety when mixins read off main thread while
     // server updates it
+    public static volatile boolean enableCustomResistanceFormula = false;
     public static volatile double resistanceModifier = EffectsConfig.resistanceModifier;
     public static volatile float regenerationAmount = EffectsConfig.regenerationAmount;
     public static volatile boolean enableMaxHealthRegen = EffectsConfig.enableMaxHealthRegen;
@@ -12,6 +13,7 @@ public class SyncedConfig {
 
     // Copies the standard local disk values into the active synced configuration
     public static void reset() {
+        enableCustomResistanceFormula = EffectsConfig.enableCustomResistanceFormula;
         resistanceModifier = EffectsConfig.resistanceModifier;
         regenerationAmount = EffectsConfig.regenerationAmount;
         enableMaxHealthRegen = EffectsConfig.enableMaxHealthRegen;
